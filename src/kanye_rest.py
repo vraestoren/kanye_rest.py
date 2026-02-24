aeceaa7b9ac53ad2a900f78a8a2ddb24375df30a
huok <77536370+vraestoren@users.noreply.github.com>
2026-02-24 15:28:10 +0300

    Update kanye_rest.py

--- src/kanye_rest.py
+++ src/kanye_rest.py
@@ -1,22 +1,19 @@
-from requests import get
+from requests import Session
 
 class KanyeRest:
 	def __init__(self) -> None:
 		self.api = "https://api.kanye.rest"
-		self.headers = {
-			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+		self.session = Session()
+		self.session.headers = {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
 	def get_random_quote(self) -> dict:
-		return get(
-			self.api, headers=self.headers).json()
+		return self.session.get(self.api).json()
 
 	def get_random_quote_as_text(self) -> str:
-		return get(
-			f"{self.api}/text",
-			headers=self.headers).text
+		return self.session.get(f"{self.api}/text").text
 
 	def get_all_quotes(self) -> dict:
-		return get(
-			"https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json",
-			headers=self.headers).json()
+		return self.session.get(
+			"https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json").json()
