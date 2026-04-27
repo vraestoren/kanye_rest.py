4c276aa00d97238a958615b8bf77628ebf2df4fa
huok <77536370+vraestoren@users.noreply.github.com>
2026-04-27 11:31:36 +0300

    Update kanye_rest.py

--- src/kanye_rest.py
+++ src/kanye_rest.py
@@ -1,4 +1,4 @@
-from requests import Session
+from requests import Session, Response
 
 class KanyeRest:
 	def __init__(self) -> None:
@@ -8,12 +8,15 @@ class KanyeRest:
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 		}
 
+	def _get(self, endpoint: str) -> Response:
+		return self.session.get(endpoint)
+
 	def get_random_quote(self) -> dict:
-		return self.session.get(self.api).json()
+		return self._get(self.api).json()
 
 	def get_random_quote_as_text(self) -> str:
-		return self.session.get(f"{self.api}/text").text
+		return self._get(f"{self.api}/text").text
 
 	def get_all_quotes(self) -> dict:
-		return self.session.get(
+		return self._get(
 			"https://raw.githubusercontent.com/ajzbc/kanye.rest/master/quotes.json").json()
